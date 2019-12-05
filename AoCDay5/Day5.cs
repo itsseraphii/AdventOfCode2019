ffc28ebe0c6dd7a64c6ae97b3ef7d70d155136b7
itsseraphii <itsseraphii@gmail.com>
2019-12-05 15:12:47 -0500

    Day5 Part 2

--- AoCDay5/Day5.cs
+++ AoCDay5/Day5.cs
@@ -16,6 +16,10 @@ namespace AoCDay5
         const int OUTPUT = 4;
         const int PARAM_POS = 0;
         const int PARAM_IMMI = 1;
+        const int JUMP_IF_TRUE = 5;
+        const int JUMP_IF_FALSE = 6;
+        const int LESS_THAN = 7;
+        const int EQUALS = 8;
 
         static void Main(string[] args)
         {
@@ -45,26 +49,27 @@ namespace AoCDay5
                 }
                 //01101
 
-
                 //EDCBA <- iInstFormat
                 //DE = OPCODE
                 //A = param3, B = param2, C = param1
                 int opCode = int.Parse(string.Format("{0}{1}", iInst[1], iInst[0]));
 
                 int numParams = 0, iParam1, iParam2, paraWrite;
+                int startInsValue = instruction;
+
+                //Set Values of params
+                if (iInst[2] == PARAM_IMMI)
+                    iParam1 = iData[instruction + 1];
+                else
+                    iParam1 = iData[iData[instruction + 1]];
 
 
                 switch (opCode)
                 {
                     case ADD:
                         numParams = 3;
-                        //Set values of params
-                        if (iInst[2] == 1)
-                            iParam1 = iData[instruction + 1];
-                        else
-                            iParam1 = iData[iData[instruction + 1]];
 
-                        if (iInst[3] == 1)
+                        if (iInst[3] == PARAM_IMMI)
                             iParam2 = iData[instruction + 2];
                         else
                             iParam2 = iData[iData[instruction + 2]];
@@ -75,12 +80,8 @@ namespace AoCDay5
                         break;
                     case MULTIPLY:
                         numParams = 3;
-                        if (iInst[2] == 1)
-                            iParam1 = iData[instruction + 1];
-                        else
-                            iParam1 = iData[iData[instruction + 1]];
 
-                        if (iInst[3] == 1)
+                        if (iInst[3] == PARAM_IMMI)
                             iParam2 = iData[instruction + 2];
                         else
                             iParam2 = iData[iData[instruction + 2]];
@@ -92,24 +93,76 @@ namespace AoCDay5
                         break;
                     case STORE:
                         numParams = 1;
+                        paraWrite = iData[instruction + 1];
+
                         Console.WriteLine("Enter value to store : ");
                         int val = int.Parse(Console.ReadLine());
-
-                        paraWrite = iData[instruction + 1];
                         iData[paraWrite] = val;
                         break;
+
                     case OUTPUT:
                         numParams = 1;
-                        if (iInst[2] == 1)
-                            iParam1 = iData[instruction + 1];
-                        else
-                            iParam1 = iData[iData[instruction + 1]];
-
                         Console.WriteLine("Output = " + iParam1);
+                        break;
+
+                    case JUMP_IF_TRUE:
+                        numParams = 2;
+
+                        if (iInst[3] == PARAM_IMMI)
+                            iParam2 = iData[instruction + 2];
+                        else
+                            iParam2 = iData[iData[instruction + 2]];
+
+                        if (iParam1 != 0)
+                            instruction = iParam2;
+                        break;
+
+                    case JUMP_IF_FALSE:
+                        numParams = 2;
+
+                        if (iInst[3] == PARAM_IMMI)
+                            iParam2 = iData[instruction + 2];
+                        else
+                            iParam2 = iData[iData[instruction + 2]];
+
+                        if (iParam1 == 0)
+                            instruction = iParam2;
+                        break;
+
+                    case LESS_THAN:
+                        numParams = 3;
+
+                        if (iInst[3] == PARAM_IMMI)
+                            iParam2 = iData[instruction + 2];
+                        else
+                            iParam2 = iData[iData[instruction + 2]];
+
+                        paraWrite = iData[instruction + 3];
+
+                        if (iParam1 < iParam2)
+                            iData[paraWrite] = 1;
+                        else
+                            iData[paraWrite] = 0;
+                        break;
+                    case EQUALS:
+                        numParams = 3;
+
+                        if (iInst[3] == PARAM_IMMI)
+                            iParam2 = iData[instruction + 2];
+                        else
+                            iParam2 = iData[iData[instruction + 2]];
+
+                        paraWrite = iData[instruction + 3];
+
+                        if (iParam1 == iParam2)
+                            iData[paraWrite] = 1;
+                        else
+                            iData[paraWrite] = 0;
                         break;
                 }
 
-                instruction += numParams + 1;
+                if(startInsValue == instruction)
+                    instruction += numParams + 1;
             }
             return iData[0];
         }
@@ -121,8 +174,6 @@ namespace AoCDay5
             for (int i = 0; i < sData.Length; i++)
             {
                 iData[i] = int.Parse(sData[i]);
-                if (iData[i] < 0)
-                    Console.WriteLine("negative");
             }
 
             return iData;
